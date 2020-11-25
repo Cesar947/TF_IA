65be9042164b84ba573abda3d17009d5a881676a
Sebastian <u201711033@upc.edu.pe>
2020-11-24 19:58:30 -0500

    tweets integrados

--- twitter_emotions/models/app.py
+++ twitter_emotions/models/app.py
@@ -12,10 +12,11 @@ class App():
         self.sbar = SearchBar(70,140)
         self.tbar = ToolBar(0,20)
         self.btn = Button(480,200)
-        self.good_option = Option(1,200,400)
-        self.bad_option = Option(2,400,400)
+        self.good_option = Option(1,200,550)
+        self.bad_option = Option(2,400,550)
         self.searched = False
-        self.tweet_detail = TweetDetail(210,260)
+        self.tweet_detail = TweetDetail(120,260)
+        self.tweet_id = ''
 
     def draw_app(self):
         self.sbar.draw_search_bar(self.window)
@@ -28,7 +29,6 @@ class App():
 
     def search(self, pos):
         self.sbar.is_clicked(pos)
-        #self.sbar.not_clicked(pos)
 
     def search_text_write(self, event):
         self.sbar.write_search_text(event)
@@ -44,5 +44,6 @@ class App():
 
     def click_button(self, pos):
         self.btn.is_over(pos)
-        self.tweet_detail.get_tweet("1330883685470572544")
+        self.tweet_id = self.sbar.get_tweet_id()
+        self.tweet_detail.get_tweet(self.tweet_id)
         self.searched = True
--- twitter_emotions/models/tweet_detail.py
+++ twitter_emotions/models/tweet_detail.py
@@ -1,29 +1,38 @@
 from decouple import config
 import tweepy
 import pygame
+import pygame.freetype
 import urllib.request
+import emoji
 import io
+import os
 from models.component import Component
+from models.util import crop_image
 
 consumer_key = config('TWITTER_API_KEY')
 consumer_secret = config('TWITTER_API_SECRET_KEY')
 access_token = config('TWITTER_ACCESS_TOKEN')
 access_token_secret = config('TWITTER_ACCESS_TOKEN_SECRET')
 
+pygame.freetype.init()
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+fontdir = os.path.dirname(os.path.abspath (__file__))
+
 pygame.init()
 
 pos = pygame.image.load('./twitter_emotions/assets/positive.png')
 neg = pygame.image.load('./twitter_emotions/assets/negative.png')
 
-font_user = pygame.font.Font('./twitter_emotions/assets/font_family/GothamNarrow-Light.otf', 15)
-font_name = pygame.font.Font('./twitter_emotions/assets/font_family/GothamNarrow-Medium.otf', 20)
-font_text = pygame.font.Font('./twitter_emotions/assets/font_family/GothamNarrow-Book.otf', 20)
+font = pygame.font.Font('./twitter_emotions/models/font_family/seguiemj.ttf', 18)
+
+font_user = pygame.freetype.Font(os.path.join (fontdir, "font_family", "GothamNarrow-Light.otf"), 18, font_index=0, resolution=0, ucs4=False)
+font_name = pygame.freetype.Font(os.path.join (fontdir, "font_family", "seguiemj.ttf"), 20, font_index=0, resolution=0, ucs4=False)
+font_text = pygame.freetype.Font(os.path.join (fontdir, "font_family", "seguiemj.ttf"), 18, font_index=0, resolution=0, ucs4=False)
 
 class TweetDetail(Component):
     def __init__(self, x, y, width = pos.get_rect().width, height = pos.get_rect().height):
@@ -34,32 +43,44 @@ class TweetDetail(Component):
         self.tweet_user_image = ''
         self.file_image = None
         self.photo_profile = None
+        self.photo_profile2 = None
 
-    def draw_tweet(self, window):           
-        #window.blit(self.photo_profile, (self.x, self.y))
-        self.image = pygame.Surface([200,200])
-        self.image.fill((255,255,255))
-        self.rect = self.image.get_rect()
-        pygame.draw.circle(self.image, (0,0,0), (self.x + 20, self.y + 10), 23)
+    def draw_tweet(self, window):     
+        window.blit(self.photo_profile, (self.x, self.y))
         #User
-        user_text = font_name.render(self.tweet_user_name, 1, (0,0,0))
-        window.blit(user_text, (self.x + 80, self.y))
+        user_text, _ = font_name.render(self.tweet_user_name,  fgcolor=None, bgcolor=None, style=pygame.freetype.STYLE_DEFAULT, rotation=0, size=0)
+        window.blit(user_text, (self.x + 55, self.y+5))
         #User name
-        name_text = font_user.render("@" + self.tweet_user, 1, (127,143,158))
-        window.blit(name_text, (self.x + 80, self.y + 20))
+        name_text, _ = font_user.render("@" + self.tweet_user, fgcolor=(127,143,158), bgcolor=None, style=pygame.freetype.STYLE_DEFAULT, rotation=0, size=0)
+        window.blit(name_text, (self.x + 55, self.y + 28))
         #Tweet text
-        tweet_text = font_text.render(self.tweet_text, 1, (0,0,0))
-        window.blit(tweet_text, (self.x, self.y+50))
+        self.blit_text(window, self.tweet_text, (self.x+5, self.y+70))
         
     def get_tweet(self, tweet_id): 
-        tweet = api.get_status(tweet_id)
+        tweet = api.get_status(tweet_id, tweet_mode='extended')
         self.tweet_user_name = tweet.user.name
         self.tweet_user_image = tweet.user.profile_image_url
         self.tweet_user = tweet.user.screen_name
-        self.tweet_text = tweet.text
+        self.tweet_text = tweet.full_text
         self.file_image = io.BytesIO(urllib.request.urlopen(self.tweet_user_image).read())
-        self.photo_profile = pygame.image.load(self.file_image)
+        mode, size, data = crop_image(self.tweet_user_image)
+        self.photo_profile = pygame.image.fromstring(data, size, mode)
 
+    def blit_text(self, surface, text, pos):
+        words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
+        space = font.size(' ')[0]  # The width of a space.
+        x, y = pos
+        for line in words:
+            for word in line:
+                word_surface , _ = font_text.render(word, fgcolor=None, bgcolor=None, style=pygame.freetype.STYLE_DEFAULT, rotation=0, size=0)
+                word_width, _ = word_surface.get_size()
+                if x + word_width >= 600:
+                    x = pos[0]  # Reset the x.
+                    y += 24  # Start on new row.
+                surface.blit(word_surface, (x, y))
+                x += word_width + space
+            x = pos[0]  # Reset the x.
+            y += 24 # Start on new row.
 
 
 
